f4d4f10913ee70800acaaf271ee37b9b60a66f69
DocN <031247ryan@gmail.com>
2018-10-08 15:47:01 -0700

    updated ef

---
+++ Lab3/Data/DummyData.cs
@@ -0,0 +1,35 @@
+﻿using Lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Data
+{
+    public class DummyData
+    {
+        public static List<City> GetCities()
+        {
+            List<City> cities = new List<City>
+            {
+                new City() {
+                CityId=01,
+                CityName="Vancouver",
+                Population=100
+                },
+                new City() {
+                CityId=02,
+                CityName="Richmond",
+                Population=200
+                },
+                new City() {
+                CityId=03,
+                CityName="Burnaby",
+                Population=200
+                }
+            };
+
+            return cities;
+        }
+    }
+}
---
+++ Lab3/Data/Migrations/20181002045751_AddSomethingToModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab3.Data.Migrations
+{
+    public partial class AddSomethingToModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
